04f856d67d6f335d2d1eb08ec49ba314ab97e2aa
carlosgg00 <garciaariel@unah.hn>
2023-01-19 17:10:35 -0600

    variables de entorno

--- database/config.js
+++ database/config.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const dbConnection = async() => {
 
     try {
-        await mongoose.connect('mongodb+srv://mern_user:ignororoZGzpFsiB@calendardb.qhhuu1b.mongodb.net/test');
+        await mongoose.connect( process.env.DB_CNN );
 
         console.log('DB Online')
 
